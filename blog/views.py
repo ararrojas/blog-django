2d777928eb83789330af5a5109f14b4c1a567c9d
ararrojas <araceli.rojas123@gmail.com>
2024-09-29 21:12:40 +0200

    migrations

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Ara',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date': 'August 27, 2018'
-    },
-    {
-        'author': 'Bebito',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date': 'August 27, 2018'
-    },
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()  # pylint: disable=no-member
     }
     return render(request, 'blog/home.html', context)
 
